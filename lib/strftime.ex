8f345f19a2ac63cedecf164161a087c79526e179
Martin Svalin <martin@lite.nu>
2018-05-14 22:54:19 +0200

    Project-wide `mix format`

--- lib/strftime.ex
+++ lib/strftime.ex
@@ -4,6 +4,7 @@ defmodule Strftime do
   defmacro defformat(name, format) do
     parsed = Format.parse(format)
     {head, body} = Format.compile(parsed)
+
     quote do
       def unquote(name)(unquote(head)), do: unquote(body)
     end
@@ -12,6 +13,7 @@ defmodule Strftime do
   defmacro defformatp(name, format) do
     parsed = Format.parse(format)
     {head, body} = Format.compile(parsed)
+
     quote do
       defp unquote(name)(unquote(head)), do: unquote(body)
     end
@@ -26,11 +28,10 @@ defmodule Strftime do
     iso_date: "%Y-%m-%d",
     iso_time: "%H:%M:%S",
     iso_datetime: "%Y-%m-%dT%H:%M:%S%:z",
-    iso_naive_datetime: "%Y-%m-%dT%H:%M:%S",
+    iso_naive_datetime: "%Y-%m-%dT%H:%M:%S"
     # rfc822: "%a, %d %b %y %H:%M:%S %z",
     # rfc2822: "%a, %d %b %Y %H:%M:%S %z",
     # httpdate: "%a, %d %b %Y %H:%M:%S %Z",
-
   ]
 
   for {name, format} <- @common_formats do
